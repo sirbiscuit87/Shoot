bccf61a08e2fd28524d34eb75a5ce41ae0f2ba47
sirbiscuit87@gmail.com <sirbiscuit87@gmail.com>
2025-03-02 21:31:07 -0800

    cursor cleanup

--- shoot/Scripts/camera.gd
+++ shoot/Scripts/camera.gd
@@ -2,8 +2,7 @@ extends Camera2D
 
 
 #References
-@export var character: CharacterBody2D #Character to follow, set by World node
-@onready var crosshair = $"../Crosshair"
+@export var character: CharacterBody2D #Character to follow
 
 #Config
 @export var max_Zoom: float #closest
@@ -21,11 +20,11 @@ func _process(delta):
 		zoom.y += .1
 	if Input.is_action_just_pressed("zoom_out") && zoom.x >= min_Zoom:
 		zoom.x -= .1
-		zoom.y -= .1	
+		zoom.y -= .1
 
 func aimHandler(delta):
 	if Input.is_action_pressed("aim"):
-		var lerpedVec = lerp(character.position, crosshair.position, 0.5)
+		var lerpedVec = lerp(character.position, get_global_mouse_position(), 0.5)
 		position.x = clamp(lerpedVec.x, character.position.x - character.view_distance, character.position.x + character.view_distance)
 		position.y = clamp(lerpedVec.y, character.position.y - character.view_distance, character.position.y + character.view_distance)
 	else:
