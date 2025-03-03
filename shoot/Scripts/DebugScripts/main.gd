5755fdcd1b238360ffe5170bbb01b1bef0e76804
sirbiscuit87@gmail.com <sirbiscuit87@gmail.com>
2025-03-02 20:24:59 -0800

    debug assistance

---
+++ shoot/Scripts/DebugScripts/main.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+# Temporary main script for this temporary main scene. 
+# This is just a playground for game mechanics, will eventually make an official setup, avoid adding logic here - Victor
