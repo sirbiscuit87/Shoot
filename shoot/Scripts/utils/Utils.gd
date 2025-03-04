b98a59c980b5120bd3dce7ee9bfbf75f15d45980
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-03-03 19:13:33 -0800

    added utility to serialize and deserialize objects for rpc

---
+++ shoot/Scripts/utils/Utils.gd
@@ -0,0 +1,14 @@
+const PROPERTY_FLAG = 4096
+
+static func obj_to_dict(obj: Object) -> Dictionary:
+    var obj_as_dict = {}
+    for prop in obj.get_property_list():
+        if prop["usage"] == PROPERTY_FLAG:
+            obj_as_dict[prop["name"]] = obj.get(prop["name"])
+
+    return obj_as_dict
+
+static func dict_to_obj(obj: Object, dict: Dictionary):
+    for key in dict.keys():
+        obj[key] = dict[key]
+    return obj
