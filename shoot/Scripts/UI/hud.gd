fe07b51669828dc1fbcd73ec4a5ae0072b277821
sirbiscuit87@gmail.com <sirbiscuit87@gmail.com>
2025-03-04 17:15:49 -0800

    gun!

---
+++ shoot/Scripts/UI/hud.gd
@@ -0,0 +1,5 @@
+extends Control
+
+# All signals from the player are sent here to be managed. Appropriate calls will be propogated down.
+func _on_player_picked_up_gun(gun: Gun) -> void:
+	$Ammo.initialize(gun) # Ammo script connects gun signals to appropriate methods.
